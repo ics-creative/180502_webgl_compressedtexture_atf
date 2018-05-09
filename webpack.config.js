2fe46bdd6b37227ec9a71b9edf07b914d2416451
ics-ikeda <ikeda@ics-web.jp>
2018-05-09 14:05:42 +0900

    最新版をビルド

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpackConfig = {
   entry: './src/Main.ts',
   // ファイルの出力設定
   output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist/js/`,
     publicPath: 'js/',
     // 出力ファイル名
     filename: 'script.js'
